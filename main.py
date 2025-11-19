0c7e3f4d7ed124f14956d5728606ecf94866f3bc
AdamHubYT <125209143+AdamHubYT@users.noreply.github.com>
2025-11-19 15:46:36 +0300

    main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+import os
+from background import keep_alive #импорт функции для поддержки работоспособности
+import pip
+pip.main(['install', 'pytelegrambotapi'])
+import telebot
+import time
+
+bot = telebot.TeleBot('8211828679:AAH6tXfjPoTVaepYMdmMSAxruUloXTgAhCE')
+
+@bot.message_handler(content_types=['text'])
+def get_text_message(message):
+  bot.send_message(message.from_user.id,message.text)
+# echo-функция, которая отвечает на любое текстовое сообщение таким же текстом   
+
+keep_alive()#запускаем flask-сервер в отдельном потоке. Подробнее ниже...
+bot.polling(non_stop=True, interval=0) #запуск бота
